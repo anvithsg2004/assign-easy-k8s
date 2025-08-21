2e50706e144133b40f3dabebb35546b451fd9f21
Anvith S G <anvithsg2004@gmail.com>
2025-08-21 12:15:37 +0530

    Removed hardcoded values and added environment variables

--- Task-Assignment/submission-service/src/main/java/com/anvith/submission_service/controller/GatewayHealthController.java
+++ Task-Assignment/submission-service/src/main/java/com/anvith/submission_service/controller/GatewayHealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GatewayHealthController {
 
-    @GetMapping("/gatewaysub/health")
+    @GetMapping("/gateway/health")
     public String healthCheck() {
         return "API Gateway is up and running!";
     }
--- Task-Assignment/user-service/src/main/java/com/anvith/user_service/config/JwtConstant.java
+++ Task-Assignment/user-service/src/main/java/com/anvith/user_service/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.anvith.user_service.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "wevvaevbbbabvdfvbvdafbvdafvbadbvadfvbadfvdfvafafvvf";
+    public static final String SECRET_KEY = "KbPeShVmYq3s6v9y$B&E)H+MbQeThWmZq4t7w!z%C*F-JaNdRgUjXn2r5u8x/A?D";
     public static final String JWT_HEADER = "Authorization";
 }
